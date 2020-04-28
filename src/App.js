412c876099d8963ae633ec1f49f4a3a4b22d6a5a
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-04-27 18:26:28 -0700

    more basic layout

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ function App() {
     <div className="App">
       <h1>Danny Byrne</h1>
         <div className="NavContainer">
-          <div className="NavButton col1" >Software Developer</div>
-          <div className="NavButton col2" >Writer</div>
-          <div className="NavButton col3" >Musician</div>
+          <div className="NavButton col1">Software Developer</div>
+          <div className="NavButton col2">Writer</div>
+          <div className="NavButton col3">Musician</div>
         </div>
-        <div className="WebBox">
-        <object type="text/html" data="http://www.hookd.dev">
-        </object>
+        <div className="SampleBox">
+          <object className="Sample" type="text/html" data="http://www.hookd.dev">
+          </object>
+          Hookd is a web-app, npm module, and CLI tool for transforming React components. 
+          It was developed using Babel, Typescript, and React. 
         </div>
     </div>
   );
