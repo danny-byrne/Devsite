442945a2a4a4dab4486d49709a60273ae4ab9ae9
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-04-27 17:37:04 -0700

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Danny Byrne</h1>
+        <div className="NavContainer">
+          <div className="NavButton col1" >Software Developer</div>
+          <div className="NavButton col2" >Writer</div>
+          <div className="NavButton col3" >Musician</div>
+        </div>
+        <div className="WebBox">
+        <object type="text/html" data="http://www.hookd.dev">
+        </object>
+        </div>
     </div>
   );
 }
