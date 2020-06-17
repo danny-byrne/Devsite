9f59331e01ed1a9c8b3c2bdc3f4c3c5357e5e2f0
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-06-17 14:13:49 -0700

    responsive css position implemented

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   console.log('app rendered')
   const [view, setView] = useState('about');
 
+
   const curView = (view) => { 
     console.log('curView is', view)
     switch (view) {
@@ -28,16 +29,16 @@ function App() {
   
 
   return (
-    <div className="App">
+    <div className="App fade-in">
       <div className="Headline">
         <h1>Danny Byrne</h1>
             <h3>Software Developer</h3>
       </div>
-        <div className="NavContainer">
-          <div className="NavButton" value="about" onClick={(e) => switchView(e, "about")}>About Me</div>
-          <div className="NavButton" value="work" onClick={(e) => switchView(e, "work")}>Work</div>
-          <div className="NavButton" value="musician" onClick={(e) => switchView(e, "musician")}>Musician</div>
-        </div>
+      <div className="NavContainer fade-in" >
+        <div className="NavButton" value="about" onClick={(e) => switchView(e, "about")}>About</div>
+        <div className="NavButton" value="work" onClick={(e) => switchView(e, "work")}>Work</div>
+        <div className="NavButton" value="musician" onClick={(e) => switchView(e, "musician")}>Creative</div>
+      </div>
       <div className="view">{curView(view)}</div>
       {/* <Work /> */}
 
@@ -48,7 +49,8 @@ function App() {
 
 const About = () => {
   console.log('rendering About View');
-  let text=`Danny Byrne is a software developer, musician, and ceremonialist`;
+  let text=`Danny Byrne is a software developer, musician, artist, and technologist across many mediums. 
+  He enjoys exploring new ideas, personal transformational practices, and the wild places of the American West.`;
 
   return (
     <div className="text">{text}</div>
@@ -61,7 +63,7 @@ const Work = () => {
     <div className="SampleBox">
       <object className="Sample" type="text/html" data="http://www.hookd.dev"></object>
       Hookd is a web-app, npm module, and CLI tool for transforming React components. 
-      It was developed using Babel, Typescript, and React. My particular role in this developer tool was creating and algorithm
+      It was developed using Babel, Typescript, and React. My particular role in this developer tool was creating an algorithm
       for detecting component structure where ContextAPI was used, and transform AST Nodes to implement useContext accordingly.
     </div>
   )
