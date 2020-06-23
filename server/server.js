7134753485f6fa9d7656272e7aed92eda250321e
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-06-23 16:28:58 -0700

    refactored server file to serve the build folder

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const publicPath = path.join(__dirname, '..', 'build');
 
-const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath));
 
