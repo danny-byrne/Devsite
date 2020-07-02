2e8b350328c64e932561352860d4a33f6a960f08
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-07-02 12:41:12 -0700

    cleared console logs, implemented constants

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, {useState} from 'react';
 import './App.css';
+import * as Constants from "./constants";
 // import SocialBadge from 'react-social-badge';
 
 const image = require('./assets/images/Danny1.jpg');
 
 function App() {
-  console.log('app rendered')
   const [view, setView] = useState('about');
-
-
   const curView = (view) => { 
-    console.log('curView is', view)
     switch (view) {
       case 'about':
          return <About />
@@ -24,8 +21,6 @@ function App() {
   }
 
   const switchView = (e, target) => {
-    // let  value  = e.target.value;
-    console.log('switchView clicked:', target)
     setView(target)
   }
 
@@ -50,21 +45,7 @@ function App() {
 }
 
 const About = () => {
-  console.log('rendering About View');
-  let text=`Danny Byrne is a software developer, technologist, and artist across many mediums. 
-  He enjoys exploring new ideas, engaging in practices in personal transformation, and seeking out the wild places of the American West.`;
-
-  const skills = [
-   { t: 'Languages',
-    c: 'Javascript(ES6+), Typescript,  HTML5/CSS3' },
-    { t: 'Front End',
-    c: 'React(Hooks), Redux, SASS/SCSS, Webpack, Babel' },
-    { t: 'Back End',
-    c: 'Node.js, Express.js, RESTful, SQL, NoSQL' },
-    { t: 'Miscellaneous',
-    c: 'Websockets, TDD, AWS(ES2, Elastik Beanstalk, DynamoDB), CI/CD, OAuth' }
-  ]
-
+  const { skills, text } = Constants;
   const skillList = skills.map(e => {
     const { t, c } = e;
     return (
@@ -91,10 +72,8 @@ const About = () => {
 }
 
 const Work = () => {
-  console.log('rendering Work View');
   return (
     <div className="SampleBox">
-      {/* <object className="Sample" type="text/html" data="http://www.hookd.dev"></object> */}
       <iframe className="Sample" type="text/html" src="https://www.hookd.dev/" title="HOOKD"></iframe>
       <a href="www.hookd.dev">Hookd</a> is a web-app, npm module, and CLI tool for transforming React components. 
       It was developed using Babel, Typescript, and React. One particular contribution of mine to this tool was creating an algorithm
@@ -104,7 +83,6 @@ const Work = () => {
 }
 
 const Musician = () => {
-  console.log('rendering Musician View');
   return (
     <div>Coming Soon...</div>
   )
---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+  export const skills = [
+   { t: 'Languages',
+    c: 'Javascript(ES6+), Typescript,  HTML5/CSS3' },
+    { t: 'Front End',
+    c: 'React(Hooks), Redux, SASS/SCSS, Webpack, Babel' },
+    { t: 'Back End',
+    c: 'Node.js, Express.js, RESTful, SQL, NoSQL' },
+    { t: 'Miscellaneous',
+    c: 'Websockets, TDD, AWS(ES2, Elastik Beanstalk, DynamoDB), CI/CD, OAuth' }
+  ]
+
+  export const text = `Danny Byrne is a software developer, technologist, and artist across many mediums. 
+  He enjoys exploring new ideas, engaging in practices in personal transformation, and seeking out the wild places of the American West.`;
+  
