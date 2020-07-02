9157a5230fc8ac31a5c5121d184f5189f3af11c8
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-07-01 17:39:53 -0700

    fixed typo

--- src/App.js
+++ src/App.js
@@ -95,8 +95,8 @@ const Work = () => {
   return (
     <div className="SampleBox">
       {/* <object className="Sample" type="text/html" data="http://www.hookd.dev"></object> */}
-      <iframe className="Sample" type="text/html" src="http://www.hookd.dev/" title="HOOKD"></iframe>
-      <a href="www.hood.dev">Hookd</a> is a web-app, npm module, and CLI tool for transforming React components. 
+      <iframe className="Sample" type="text/html" src="https://www.hookd.dev/" title="HOOKD"></iframe>
+      <a href="www.hookd.dev">Hookd</a> is a web-app, npm module, and CLI tool for transforming React components. 
       It was developed using Babel, Typescript, and React. One particular contribution of mine to this tool was creating an algorithm
       for detecting component structure where ContextAPI was used, and to conditionally modify AST Nodes to inject useContext and other hooks accordingly.
     </div>
